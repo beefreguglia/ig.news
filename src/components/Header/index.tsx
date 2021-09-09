7f7af9d7d957281aee8fad358ac2a16fc2afff79
Bernardo <bernardo@bernardo-b.freguglia>
2021-09-09 13:02:04 -0300

    create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss';
+import { SignInButton } from './SignInButton/index';
+
+export function Header(){
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignInButton  />
+      </div>
+    </header>
+  );
+}
