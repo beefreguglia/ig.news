0cd1bbba8d9c6e9cb3212f6c7c8b1b4fdd99120d
beefreguglia <beefreguglia@gmail.com>
2021-09-11 17:20:29 -0300

    Adicionando pagamentos ao stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +8,29 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId: string }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe(){
+    if(!session){
+      signIn('github')
+      return;
+    }
+    //Criaçao da Checkout session
+    try{
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      stripe.redirectToCheckout( { sessionId } );
+    } catch (e){
+      alert(e.message);
+    }
+  }
+
   return(
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe Now
     </button>
